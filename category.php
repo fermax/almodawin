26abf41fd1f15b651d727e4259d64715e325f967
fermax <adabihbb@gmail.com>
2016-10-07 19:41:02 +0100

    إضافة تعليق توضيحي
    أضفت تعليقا توضيحيا للكود

--- category.php
+++ category.php
@@ -4,6 +4,7 @@
     $cat_slug = $_GET["cat_slug"];
     $cat_slug = filter_input(INPUT_GET, "cat_slug", FILTER_SANITIZE_SPECIAL_CHARS);
     echo "<h2 class=\"btn btn-success\">جميع فوائد قسم : ".$Posts->categoryPosts($cat_slug)[0]["cname"]."</h2>";
+    // هذه تجربة للمخرجات
     var_dump($Posts->categoryPosts($cat_slug));
     include "footer.php"; 
 ?>
