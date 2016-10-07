8f716cc49f5f885ced17d59192dfce52cd474e26
fermax <adabihbb@gmail.com>
2016-10-07 19:38:56 +0100

    اختصار الكود
    اختصرتُ الكود و ذلك بحذف ت وسوم ال بي اتش بي غير الضرورية .

--- category.php
+++ category.php
@@ -1,9 +1,9 @@
-<?php include "header.php";include "inc.php";?>
 <?php
-
+    include "header.php";
+    include "inc.php";
     $cat_slug = $_GET["cat_slug"];
     $cat_slug = filter_input(INPUT_GET, "cat_slug", FILTER_SANITIZE_SPECIAL_CHARS);
     echo "<h2 class=\"btn btn-success\">جميع فوائد قسم : ".$Posts->categoryPosts($cat_slug)[0]["cname"]."</h2>";
     var_dump($Posts->categoryPosts($cat_slug));
+    include "footer.php"; 
 ?>
-<?php include "footer.php"; ?>
